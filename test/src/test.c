f5c61dd8261e8a5a895379a2e09c24980b2d9df6
manolace <manolace@gmail.com>
2017-08-29 11:48:27 +0300

    commin

---
+++ test/src/test.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : test.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World+1233344!!!"); /* prints !!!Hello World!!! */
+	return 0;
+}
